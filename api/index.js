ae609b79354a518d1ac7b85a451c1cf953ef0889
Jhozefem Pontes <jhozefem@MacBook-de-Jhozefem.local>
2025-05-16 23:02:21 -0300

    backend

--- api/index.js
+++ api/index.js
@@ -15,12 +15,12 @@ const serviceAccountAuth = new JWT({
 const doc = new GoogleSpreadsheet(process.env.GOOGLE_SPREADSHEET_ID, serviceAccountAuth);
 let sheet;
 
-(async () => {
-  await doc.loadInfo();
-  sheet = doc.sheetsByIndex[0];
-})();
-
 app.get('/api/check/:phone', async (req, res) => {
+  if (!sheet) {
+    await doc.loadInfo();
+    sheet = doc.sheetsByIndex[0];
+  }
+
   const phone = req.params.phone;
   await sheet.loadCells();
   const rows = await sheet.getRows();
@@ -32,6 +32,11 @@ app.get('/api/check/:phone', async (req, res) => {
 });
 
 app.post('/api/respond', async (req, res) => {
+  if (!sheet) {
+    await doc.loadInfo();
+    sheet = doc.sheetsByIndex[0];
+  }
+
   const { phone, answer } = req.body;
   const rows = await sheet.getRows();
   const row = rows.find(r => r.Telefone === phone);
