96a72fdc73ecc32c9f93088b45f99e75af20a8bb
Jhozefem Pontes <jhozefem@MacBook-de-Jhozefem.local>
2025-05-16 22:44:28 -0300

    backend

--- api/index.js
+++ api/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 const { JWT } = require('google-auth-library');
-require('dotenv').config();
 
 const app = express();
 app.use(express.json());
